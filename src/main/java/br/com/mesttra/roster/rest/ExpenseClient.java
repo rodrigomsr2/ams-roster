69e858456fff70abc6e1c463d19c9f35e119cb75
rodrigomsr2 <rodrigomsr2@gmail.com>
2021-10-20 20:03:28 -0300

    salvar antes de mudar de máquina

--- src/main/java/br/com/mesttra/roster/rest/ExpenseClient.java
+++ src/main/java/br/com/mesttra/roster/rest/ExpenseClient.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "montlyExpense", url="http://localhost:8083/")
+@FeignClient(name = "expenses", url="http://localhost:8083/")
 public interface ExpenseClient {
 
-	@RequestMapping("/montlyExpense")
+	@RequestMapping("/expenses")
     DespesaDTO sendMail(@RequestBody DespesaDTO despesaDTO);
 
 }
